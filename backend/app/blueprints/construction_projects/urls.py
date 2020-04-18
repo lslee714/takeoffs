2d88407f877ea987ad00c29c9285e3c73a07b009
Luke Lee <lslee714@gmail.com>
2020-04-18 10:24:19 -0600

    Add additional packages, make a simple get to allow http GET calls to

--- backend/app/blueprints/construction_projects/urls.py
+++ backend/app/blueprints/construction_projects/urls.py
@@ -1,7 +1,11 @@
+from flask import jsonify
+
+
 def register(blueprint):
     """Register the routes for the blueprint"""
+
     @blueprint.route('/')
     def index():
         """Root for the constructionProjects blueprint"""
-        return "Hello another world"
-        
+        return jsonify({'message': 'Hello world from construction projects'})
+            
