12d3caa3cf9633154797af6dc46974a29413a0cf
Luke Lee <lslee714@gmail.com>
2020-04-18 10:37:51 -0600

    set up bootstrap/redux, proof of concept

---
+++ frontend/src/components/construction-projects/ConstructionProjects.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Alert from 'react-bootstrap/Alert';
+
+import './ConstructionProjects.css';
+
+function ConstructionProjects() {
+  return <Alert variant="success">Hey is this making sense yet</Alert>;
+}
+
+export default ConstructionProjects;
