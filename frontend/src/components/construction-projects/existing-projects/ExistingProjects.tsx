71e40ceca2839322f1d93911c01836cc4427be3c
Luke Lee <lslee714@gmail.com>
2020-04-18 20:07:52 -0600

    Show existing projects

--- frontend/src/components/construction-projects/existing-projects/ExistingProjects.tsx
+++ frontend/src/components/construction-projects/existing-projects/ExistingProjects.tsx
@@ -1,12 +1,31 @@
 import React from 'react';
-
 import ListGroup from 'react-bootstrap/ListGroup';
+import { useSelector } from 'react-redux';
+
+import { IConstructionProject } from '../../../models/ConstructionProject';
+import { IRootState } from '../../../store/reducers';
 
 function ExistingProjects() {
+  const existingProjects: IConstructionProject[] = useSelector(
+    (state: IRootState) => {
+      const projects = state.projects;
+      const sortedProjects: IConstructionProject[] = [];
+      projects.sorted.forEach((id) => sortedProjects.push(projects.byId[id]));
+      return sortedProjects;
+    }
+  );
+
   return (
-    <ListGroup>
-      <ListGroup.Item action>A Project</ListGroup.Item>
-    </ListGroup>
+    <div>
+      <h3>Existing Projects</h3>
+      <ListGroup>
+        {existingProjects.map((project: IConstructionProject) => (
+          <ListGroup.Item key={project.id} action>
+            {project.name}
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
+    </div>
   );
 }
 
