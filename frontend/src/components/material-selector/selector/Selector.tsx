750d6e93961af9a7aff4aa74b52c1d125b026ae1
Luke Lee <lslee714@gmail.com>
2020-04-19 14:13:08 -0600

    call get categories when loaded

--- frontend/src/components/material-selector/selector/Selector.tsx
+++ frontend/src/components/material-selector/selector/Selector.tsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useEffect, Dispatch } from 'react';
+import { useDispatch } from 'react-redux';
 
 import './Selector.scss';
 
+import MaterialSelectorActions from '../../../store/actions/material-selector';
+
 const Selector = () => {
+  const dispatch: Dispatch<any> = useDispatch();
+
+  useEffect(() => {
+    dispatch(MaterialSelectorActions.getCategories());
+  });
+
   return <div className="main">HELLO</div>;
 };
 
